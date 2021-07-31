92914b7e06710a37fccacc24d54250a344143312
jkaho <72959522+jkaho@users.noreply.github.com>
2021-07-31 23:33:05 +1000

    Remove error return value from Sqrt func

--- 1 - loops and functions/main.go
+++ 1 - loops and functions/main.go
@@ -5,21 +5,17 @@ import (
 	"math"
 )
 
-func Sqrt(x float64) (float64, error) {
-	if x < 0 {
-		return 0, fmt.Errorf("Negative numbers don't have square roots!")
-	}
+func Sqrt(x float64) float64 {
 	z := 1. // estimation of sqrt(x)
 	y := 0. // previous value of z
 	for math.Abs(z-y) > 0.00000000001 {
 		y = z
 		z -= (z*z - x) / (2 * z)
 	}
-	return z, nil
+	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(-2))
 	fmt.Println(Sqrt(2))
 	fmt.Println(math.Sqrt(2)) // standard library
 }
