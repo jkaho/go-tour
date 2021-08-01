9b7a703229cef79ede5369490b74f5dd43f4206f
jkaho <72959522+jkaho@users.noreply.github.com>
2021-08-01 14:31:09 +1000

    Add 'rot13Reader' solution

---
+++ 8 - rot13Reader/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rr *rot13Reader) Read(b []byte) (int, error) {
+	n, err := rr.r.Read(b)
+	for i := 0; i < len(b); i++ {
+		l := strings.ToLower(string(b[i]))
+		if l >= "a" && l <= "m" {
+			b[i] += 13
+		} else if l > "m" && l <= "z" {
+			b[i] -= 13
+		}
+	}
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
